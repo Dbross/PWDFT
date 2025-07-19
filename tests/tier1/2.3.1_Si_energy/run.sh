98646a5f87a24e1d7306a59ce932cc263ab6f468
David Bross <dbrosswsu@gmail.com>
2025-07-19 14:40:15 -0500

    Ignore VPP, .tar.gz and tests artifacts for clean status

--- tests/tier1/2.3.1_Si_energy/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-PWDFT_EXECUTABLE=${PWDFT_BIN:-../../../build/pwdft}
-$PWDFT_EXECUTABLE < si_energy.nw > si_energy.out 
--- tests/tier1/2.3.2_Al_smearing/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-PWDFT_EXECUTABLE=${PWDFT_BIN:-../../../build/pwdft}
-$PWDFT_EXECUTABLE < al_energy.nw > al_energy.out 
