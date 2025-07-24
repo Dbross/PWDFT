7f612f4072d76a67b4d604d793707b2de8a32d5b
David Bross <dbrosswsu@gmail.com>
2025-07-23 21:40:09 -0500

    wip: systematic allocation, debug, and NaN/Inf checks for BAND H2 debug (see debug_cu_band_crash_input.md)

--- tests/tier1/2.4.1_H2_band/run.sh
+++ tests/tier1/2.4.1_H2_band/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PWDFT_EXECUTABLE=${PWDFT_BIN:-../../../build/pwdft}
 rm h2-energy.movecs
-$PWDFT_EXECUTABLE < h2_band.nw > h2_band.out 
+$PWDFT_EXECUTABLE < h2_band.nw > h2_band.out  2> debug.out
